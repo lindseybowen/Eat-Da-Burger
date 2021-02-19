41a09e017eb6a5d5a583de7fb3eee7570e184866
lindsey Bowen <lindseyjbowen@gmail.com>
2021-02-19 09:38:36 -0500

    set up code to connect to node to MySQL

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+require("dotenv").config();
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.PASS,
+    database: "burgers_db",
+  });
+}
+
+// Make connection.
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+// Export connection for ORM
+module.exports = connection;
